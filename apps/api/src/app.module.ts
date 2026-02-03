43536148f1aba8756866b7727ec4ecb6cc2ce525
Sapir Ohava <ohavasapir@gmail.com>
2026-02-03 13:45:46 +0200

    deleted default stuff in api

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,14 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { HealthController } from './health.controller';
-import { ArticlesController } from './articles.controller';
 import { PrismaModule } from './prisma/prisma.module';
 
 
 @Module({
   imports: [PrismaModule],
-  controllers: [AppController, HealthController, ArticlesController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
